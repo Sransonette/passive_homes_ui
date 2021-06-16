970680706c4c764e15c4a6cf9af8c9c0689deb46
Sean <Sransonette@gmail.com>
2021-06-15 23:08:38 -0500

    first commit

---
+++ src/components/test.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Test extends Component{
+
+
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+
+
+
+
+
+
+
+}
+
+export default Test; 
