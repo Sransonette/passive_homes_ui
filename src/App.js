a41d7b32c9d7c128d5f409538fd823d03c7b2221
Sean <Sransonette@gmail.com>
2021-06-16 22:38:28 -0500

    create container to hold each separate component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import Test from './components/test.js'
+import ButtonsContainer from './containers/ButtonsContainer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Test/>
+        <ButtonsContainer/>
       </div>
   )};
 
---
+++ src/containers/ButtonsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Test from '../components/test.js'
+
+class ButtonsContainer extends Component { 
+
+    render() {
+        return( 
+            <div>
+            <Test/>
+            </div>
+        )
+    }
+
+}
+
+export default ButtonsContainer
