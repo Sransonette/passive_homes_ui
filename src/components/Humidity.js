9754362a243a59f297bef869b317d106306fb988
Sean <Sransonette@gmail.com>
2021-07-01 22:06:27 -0500

    create new component to hold current humidity, humidity up, and humidity down

---
+++ src/components/Humidity.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Humidity extends Component {
+
+    state = {
+        hum = "50"
+    }
+
+
+
+    render() {
+        return (
+            <div className={"currentHumid"}>
+                
+            </div>
+        );
+    }
+}
+
+export default Humidity;
